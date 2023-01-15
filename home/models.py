65f2ccd9fbfadd554420d96ac5a898d8bb66a15a
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-01-15 10:02:54 +0100

    Stable nice template

--- home/models.py
+++ home/models.py
@@ -1,13 +1,53 @@
 from django.db import models
 
-from wagtail.models import Page
+# New imports added for ParentalKey, Orderable, InlinePanel
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.search import index
 
 
 class HomePage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class PagePageGalleryImage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+class ContentPage(Page):
+    body = RichTextField(blank=True)
+    hero = RichTextField(blank=True)
+    intro = models.CharField(max_length=250)
+    
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
+        FieldPanel('hero'),
+        FieldPanel('intro'),
     ]
